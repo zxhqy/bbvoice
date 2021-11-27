2b11967a059f453a89f6dfa8de61b319b06ff6d3
zxhqy <waiyongz@163.com>
2021-11-27 16:42:15 +0800

    Update home.vue

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,38 @@
 <template>
-    <el-button type="info" @click="logout">exit</el-button>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <span>蚌埠语音管理后台</span>
+      </div>
+      <el-button type="info" @click="logout">退出系统</el-button>
+    </el-header>
+
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-menu-item index="1">
+            <i class="el-icon-user"></i>
+            <span slot="title">用户概览</span>
+          </el-menu-item>
+
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">用户日程</span>
+          </el-menu-item>
+
+          <el-menu-item index="3">
+            <i class="el-icon-school"></i>
+            <span slot="title">课表管理</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 
@@ -11,16 +44,13 @@
 
 <script>
 export default {
-    data:{
-
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
     },
-    methods:{
-        logout(){
-            window.sessionStorage.clear();
-            this.$router.push('/login');
-        }
-    }
-}
+  },
+};
 </script>
 
 
@@ -32,5 +62,29 @@ export default {
 
 
 <style lang="less" scoped>
-
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 30px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.home-container {
+  height: 100%;
+}
 </style>
